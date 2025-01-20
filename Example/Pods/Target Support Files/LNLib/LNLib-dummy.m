92e50164a953395fa6e313066dec2c7e48c08cdf
szenyew <szenyewedgetech@gmail.com>
2025-01-20 10:03:46 +0800

    first init

---
+++ Example/Pods/Target Support Files/LNLib/LNLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LNLib : NSObject
+@end
+@implementation PodsDummy_LNLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-LNLib_Tests/Pods-LNLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LNLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LNLib_Tests
+@end
